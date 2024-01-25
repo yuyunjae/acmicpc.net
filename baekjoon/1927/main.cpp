6ef4532598e6e0eb4246c65b58a2ad3db6782f86
Yu Yunjae <yuyunjae@naver.com>
2024-01-25 17:49:05 +0900

    priority queue

---
+++ baekjoon/1927/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int> > p_queue;
+
+int main(void)
+{
+    int N, x;
+
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> x;
+        if (x == 0)
+        {
+            if (p_queue.empty())
+                cout << "0\n";
+            else
+            {
+                cout << p_queue.top() << '\n';
+                p_queue.pop();
+            }
+        }
+        else
+            p_queue.push(x);
+    }
+}
