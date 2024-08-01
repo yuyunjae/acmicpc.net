b9e4e7f96f8e1f5913a91cabaaf70a7c56316fd3
yuyunjae <yuyunjae@naver.com>
2024-08-02 00:48:11 +0900

    fix 1456 prime number

--- baekjoon/1456/main.cpp
+++ baekjoon/1456/main.cpp
@@ -1,3 +1,13 @@
+/* if
+{
+	//
+}
+42하면서 이런 꼴로 썼는데, 오히려 가독성이 떨어지는 것 같기도 하다.. 
+if {
+	//
+}
+이게 더 좋을 수도..
+*/
 #include <iostream>
 
 using namespace std;
@@ -6,38 +16,32 @@ bool visit[10000001];
 
 int main(void)
 {
-	unsigned long long int N, M, ans = 0, before;
+	unsigned long long int N, M, ans = 0;
 
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
 	cin >> N >> M;
 	visit[2] = true;
-	before = 4;
 	for (unsigned long long int j = 4; j <= M; j = j << 1)
 	{
-		if (before > j)
-			break ;
 		if (j >= N)
 			ans++;
-		before = j;
 	}
-	// cout << ans << endl;
+	//cout << ans << " ";
 	for (unsigned long long int i = 3; i * i <= M; i = i + 2)
 	{
 		if (!visit[i])
 		{
-			//cout << i << " ";
 			for (unsigned long long int j = i; j * j <= M; j += 2 * i)
 				visit[j] = true;
-			before = i;
+
 			for (unsigned long long int j = i * i; j <= M; j *= i)
 			{
-				if (100000000000000 / before < i)
-					break ;
 				if (j >= N)
 					ans++;
-				before = j;
+				if (M / i < j)
+					break ;
 			}
 		}
 	}
