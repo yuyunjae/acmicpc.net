15ef6bd7029141b92d6f3a7003ff6aa5d7f76de0
yuyu <yuyunjae03@gmail.com>
2024-08-10 20:26:45 +0900

    sol 24680 793 797

--- baekjoon/24680/main.cpp
+++ baekjoon/24680/main.cpp
@@ -1,3 +1,31 @@
+// 797
+// #include <iostream>
+
+// using namespace std;
+
+// int main(void)
+// {
+// 	for (int j = 0; j < 7; j++) //  7번
+// 	{
+// 		cout << "DR";
+// 		for (int i = 0; i < 14; i++) // 14번
+// 			cout << "rFr";
+// 		cout << "DL";
+// 		for (int i = 0; i < 14; i++) // 14번
+// 			cout << "lFl";
+// 	}
+// 	cout << "D";
+// 	for (int i = 0; i < 15; i++) // 15번
+// 		cout << "rFr";
+// 	for (int i = 0; i < 15; i++) // 15번
+// 		cout << "uFu";
+// 	for(int i = 0; i < 15; i++) // 15번
+// 		cout << "lFl";
+// 	for(int i = 0; i < 15; i++) // 15번
+// 		cout << "dFd";
+// }
+
+// 793
 #include <iostream>
 
 using namespace std;
@@ -13,13 +41,13 @@ int main(void)
 		for (int i = 0; i < 14; i++) // 14번
 			cout << "lFl";
 	}
-	cout << "D";
+	for (int i = 0; i < 14; i++) // 14번
+		cout << "uFu";
 	for (int i = 0; i < 15; i++) // 15번
 		cout << "rFr";
-	for (int i = 0; i < 15; i++) // 15번
-		cout << "uFu";
-	for(int i = 0; i < 15; i++) // 15번
-		cout << "lFl";
 	for(int i = 0; i < 15; i++) // 15번
 		cout << "dFd";
+	for(int i = 0; i < 15; i++) // 15번
+		cout << "lFl";
 }
+
